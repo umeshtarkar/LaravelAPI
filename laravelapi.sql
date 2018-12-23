0df5ae1aac349047791a4b054f961c2791ddaba8
root <umesh.kumar@enukesoftware.com>
2018-12-23 21:57:51 +0530

    Updated sql

--- laravelapi.sql
+++ laravelapi.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 20, 2018 at 06:35 AM
+-- Generation Time: Dec 23, 2018 at 09:56 PM
 -- Server version: 5.7.24-0ubuntu0.16.04.1
 -- PHP Version: 7.0.32-0ubuntu0.16.04.1
 
@@ -19,6 +19,81 @@ SET time_zone = "+00:00";
 --
 -- Database: `laravelapi`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `email`, `password`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'umesh.kumar@enukesoftware.com', 'umesh123', 1, '2018-12-23 10:47:09', '2018-12-23 10:51:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `subtitle` varchar(255) DEFAULT NULL,
+  `text` text,
+  `views` bigint(20) NOT NULL DEFAULT '0',
+  `picture_large` int(11) DEFAULT NULL,
+  `picture_small` int(11) DEFAULT NULL,
+  `youtube_video_url` varchar(255) NOT NULL,
+  `news_category_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`id`, `title`, `subtitle`, `text`, `views`, `picture_large`, `picture_small`, `youtube_video_url`, `news_category_id`, `user_id`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Loan Waiver', 'Loan Waiver by Narendra Modi Government', 'Centeral government recently announced', 21, NULL, NULL, 'http://www.zeenews.com/loan-waiver', 1, 1, 0, '2018-12-22 23:38:47', '2018-12-23 03:36:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news_categories`
+--
+
+CREATE TABLE `news_categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `picture` varchar(255) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `news_categories`
+--
+
+INSERT INTO `news_categories` (`id`, `name`, `picture`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Umesh', '1545576403Screenshot from 2018-12-07 07-21-07.png', 1, '2018-12-23 05:03:40', '2018-12-23 09:16:43'),
+(2, 'Kunj Bihari Lal', '1545561260JioRecharge.png', 0, '2018-12-23 05:04:20', '2018-12-23 05:04:20'),
+(3, 'social', '1545580926Screenshot from 2018-12-20 18-40-29.png', 0, '2018-12-23 10:32:06', '2018-12-23 10:32:06');
 
 -- --------------------------------------------------------
 
@@ -48,11 +123,29 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `surname`, `email`, `password`, `address`, `mobile_no`, `country_code`, `country`, `profession`, `council_reg_no`, `status`, `created_at`, `updated_at`) VALUES
-(2, 'Umesh', 'Sharma', 'uks.tarkar@gmail.com', '$2y$10$uA31Kg92X09zDBxGGJKGSeU6UjS4NUZLvF/GrO59mEdYNWijct0T.', NULL, '8750589063', NULL, NULL, 'Software Engineer', '2345', 0, '2018-12-19 18:00:54', '2018-12-19 19:20:16');
+(2, 'Umesh', 'Sharma', 'uks.tarkar@gmail.com', '$2y$10$uA31Kg92X09zDBxGGJKGSeU6UjS4NUZLvF/GrO59mEdYNWijct0T.', NULL, '8750589063', NULL, NULL, 'Software Engineer', '2345', 1, '2018-12-19 18:00:54', '2018-12-19 19:49:00');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `news_categories`
+--
+ALTER TABLE `news_categories`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -64,6 +157,21 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `news_categories`
+--
+ALTER TABLE `news_categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `users`
 --
